c65b1d0fcf143106b5231507f5b5aab9527d0807
praneeth <gp.gunturu@gmail.com>
2025-07-11 16:54:47 -0500

    added babel.config.js and using .env to store the API_KEY. Creating a new user works!

--- util/http.js
+++ util/http.js
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { API_KEY } from '@env';
 
-const api_key = process.env.API_KEY
+// const api_key = process.env.API_KEY
+// const api_key="AIzaSyCzVD--M8JjXWC2PACdme2xpmNfoZZIIKE"
 
 export async function createUser(email, password) {
-    const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${api_key}`, 
+    const response = await axios.post(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`, 
         {
             email: email,
             password: password,
