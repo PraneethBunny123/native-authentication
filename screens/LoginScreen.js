7f66f726bf00fdc95a3d7ebfc08de28d32f92f66
praneeth <gp.gunturu@gmail.com>
2025-07-12 13:36:07 -0500

    setting the token to the context

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,16 +1,20 @@
 import AuthContent from '../components/Auth/AuthContent';
 import LoadingOverlay from '../components/ui/LoadingOverlay';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { login } from '../util/http';
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
+  const authCtx = useContext(AuthContext)
+
   async function handleLogin({email, password}) {
     setIsAuthenticating(true)
     try {
-      await login(email, password)
+      const token = login(email, password)
+      authCtx.authenticate(token)
     } catch(error) {
       Alert.alert('Authentication failed, Please check your credentials or try again later')
     }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
 import { createUser } from '../util/http';
 import LoadingOverlay from '../components/ui/LoadingOverlay'
 import { Alert } from 'react-native';
+import { AuthContext } from '../store/auth-context';
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
 
+  const authCtx = useContext(AuthContext)
+
   async function handleSignup({email, password}) {
     setIsAuthenticating(true)
     try {
-      await createUser(email, password)
+      const token = createUser(email, password)
+      authCtx.authenticate(token)
     } catch(error) {
       Alert.alert('Could not create user, please try again later')
     }
