eff16cc888a756992d738f4367a723cbfcc7c6cd
praneeth <gp.gunturu@gmail.com>
2025-07-13 11:12:48 -0500

    created a dummy database and fectching the response when logged in

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen() {
+  const [messageNode, setMessageNode] = useState('')
+
+  useEffect(() => {
+    axios.get('https://native-authentication-49111-default-rtdb.firebaseio.com/message.json').
+    then((response) => {
+      setMessageNode(response.data)
+    })
+  }, [])
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{messageNode}</Text>
     </View>
   );
 }
